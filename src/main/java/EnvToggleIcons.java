b2d3991d9fbb319ca5b72de820749a12fd5aa2d5
Ben Youngblut <by@fotosearch.com>
2018-03-29 11:10:59 -0500

    Redo Icons to fit the intelij theme better
    I have created a new set of icons and also added a gray Icon that
    will be used in the future for when this plugin is not active.

--- src/main/java/EnvToggleIcons.java
+++ src/main/java/EnvToggleIcons.java
@@ -3,6 +3,7 @@ import com.intellij.openapi.util.IconLoader;
 import javax.swing.*;
 
 class EnvToggleIcons {
-    static final Icon Green = IconLoader.getIcon("/icons/EnvToggle_green.png");
-    static final Icon Red = IconLoader.getIcon("/icons/EnvToggle_red.png");
+    static final Icon Green = IconLoader.getIcon("/icons/green.png");
+    static final Icon Gray = IconLoader.getIcon("/icons/gray.png");
+    static final Icon Red = IconLoader.getIcon("/icons/red.png");
 }
